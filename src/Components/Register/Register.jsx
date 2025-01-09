d675571476b208ce47088b899fa4bff8560d1ac2
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-01-09 13:17:13 +0600

    practicing

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
+        <div className='border'>
             <h3 className="text-3xl">Please Register</h3>
+            <form >
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="password" name="password" id="" />
+                <br />
+                <input type="submit" value="register" />
+            </form>
         </div>
     );
 };
